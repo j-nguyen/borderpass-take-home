d7f97b1a862551fcc978a34f4400b6714ed3a188
Johnny Nguyen <jnguyen1236@gmail.com>
2025-10-21 22:52:24 -0400

    feat: setup delete and creation on questions

--- api/src/controllers/questions.ts
+++ api/src/controllers/questions.ts
@@ -1,6 +1,6 @@
 import { eq } from 'drizzle-orm/pg-core/expressions'
 import db from '../database/db.js'
-import { questions as questionsTable } from '../database/schema.js'
+import { questions as questionsTable, questionSelections as questionSelectionsTable } from '../database/schema.js'
 
 export const getQuestions = async(req, res) => { 
   const result = await db.query.questions.findMany({ with: { question_selections: true } })
@@ -8,13 +8,27 @@ export const getQuestions = async(req, res) => {
 }
 
 export const createQuestion = async(req, res) => {
-  await db.insert(questionsTable).values()
+  const body = {
+    title: req.body.title,
+    question_type: req.body.question_type,
+    required: req.body?.required ?? false,
+  }
+
+  const newQuestion = await db.insert(questionsTable).values(body).returning()
+
+  if (req.body.question_selections) {
+    const updatedSelections = req.body.question_selections.map(q => ({ ...q, question_id: newQuestion[0].id }))
+    console.log(updatedSelections)
+    await db.insert(questionSelectionsTable).values(updatedSelections)
+  }
+
+  res.status(201).json(newQuestion)
 }
 
 export const deleteQuestion = async(req, res) => {
   try {
-    await db.delete(questionsTable).where(eq(questionsTable.id, req.questionId))
-    res.status(204)
+    await db.delete(questionsTable).where(eq(questionsTable.id, req.params.questionId))
+    return res.status(204).json(null)
   } catch(e) {
     console.error(e)
     res.status(404).json({ message: e.message })    
