2eda006e64c3f4192d61b04c9c47df051e0caeb7
Johnny Nguyen <jnguyen1236@gmail.com>
2025-10-21 23:46:17 -0400

    feat: setup the app layout, scaffolded

--- app/app/routes/home.tsx
+++ app/app/routes/home.tsx
@@ -1,13 +1,37 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Col, Row, Container, Button, Card } from "react-bootstrap";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Questionnaire App" },
   ];
 }
 
+// Function
+function CardLayout() {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+// Export Rendering
 export default function Home() {
-  return <Welcome />;
+  return (
+    <Container fluid>
+      <Row className="mt-3">
+        <Col md={{ span: 6, offset: 3 }}>
+          <CardLayout></CardLayout>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
